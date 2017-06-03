1087066b3d56fcc83e03adff18a2ca30f28498c8
Ricardo Barbosa <barbosa.cisco@gmail.com>
2017-06-03 19:19:55 +0100

    helloworld.py

---
+++ countdown.py
@@ -0,0 +1,11 @@
+# This simple program was taken from the online course Python For Everybody, Coursera.
+# The purpose of the program is to take a number and count down until 0.
+
+
+n = 5019230910932
+
+while n > 0:
+    print n
+    n = n - 1
+
+print "Done"
---
+++ input.py
@@ -0,0 +1,12 @@
+#This is a really nice program to learn conditions, it asks for a name if name is John presents something, if it is not John presents another thing.
+
+# -*- coding: utf-8 -*-
+
+# Below we set a 
+name = raw_input("What is your name?")
+if name == "John":
+    print "Welcome",name
+else:
+    print "Welcome!"
+
+
