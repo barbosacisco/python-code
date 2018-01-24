5350d3662a3273ebe2430a9e591452df39a117ac
Ricardo Barbosa <barbosa.cisco@gmail.com>
2018-01-24 10:35:28 +0000

    Removed old lines of code.

--- area_perimetro.py
+++ area_perimetro.py
@@ -3,6 +3,3 @@
 lado = input("Insira o valor de um lado do quadrado: ")
 
 print ("O perímetro do quadrado é:", float(lado) * 4 ," e a área do quadrado é:", float(lado) ** 2) 
-#print ("A área do quadrado é:", float(lado) ** 2) 
-
-#Lado * 4 "A área é:" Lado ** 2)
