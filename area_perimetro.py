6f75fe52b31d5c8e190b742ec3724750cc5a8596
Ricardo Barbosa <barbosa.cisco@gmail.com>
2018-01-24 10:33:43 +0000

    Programa para calcular o perímetro e a área de um quadrado.

---
+++ area_perimetro.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+lado = input("Insira o valor de um lado do quadrado: ")
+
+print ("O perímetro do quadrado é:", float(lado) * 4 ," e a área do quadrado é:", float(lado) ** 2) 
+#print ("A área do quadrado é:", float(lado) ** 2) 
+
+#Lado * 4 "A área é:" Lado ** 2)
