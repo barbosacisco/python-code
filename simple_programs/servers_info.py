3215f85140aac007d596454215506d811b7872dd
Ricardo <barbosa.cisco@gmail.com>
2018-09-01 18:00:23 +0100

    added a program to ssh into a list of servers and execute specific command

---
+++ simple_programs/servers_info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# This program iterates through an IP Address list and execute a specific command in the servers. 
+
+import os
+
+ip_list = ["xxx.xxx.xxx.xxx" , "xxx.xxx.xxx.xxx"]
+
+command = "cat /etc/passwd"
+
+for ip in ip_list:
+	os.system("ssh root" + "@" + ip + " '" + command + "'")
