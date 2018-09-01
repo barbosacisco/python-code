1ab103c32fcfb9304aec2e09d58890dd4d1a7e80
Ricardo <barbosa.cisco@gmail.com>
2018-09-01 08:19:17 +0100

    added lagartuz_input.py code

---
+++ lagartuz/lagartuz_input.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+project = raw_input("What is your project? ")
+
+#print project.title()
+
+if project == "Lagartuz":
+	print "Lagartuz Project is the best!"
+	
+else:
+
+	print "..."
+
