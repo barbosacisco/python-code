b188a6a29e1f1e9df523882354ffb9b5dd81056d
Ricardo <barbosa.cisco@gmail.com>
2018-09-01 18:05:25 +0100

    added program to iterate over a list of servers executing a specific command

--- simple_programs/servers_info.py
+++ simple_programs/servers_info.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python
 
-# This program iterates through an IP Address list and execute a specific command in the servers. 
+"""
+
+ This program iterate through an IP Address list,
+ and execute a specific command in the servers. 
+
+"""
 
 import os
 
-ip_list = ["xxx.xxx.xxx.xxx" , "xxx.xxx.xxx.xxx"]
+ip_list = ["xxx.xxx.xxx.xxx" , "xxx.xxx.xxx.xxx"] # Change it for the IP Addresses of your servers. 
 
 command = "cat /etc/passwd"
 
 for ip in ip_list:
 	os.system("ssh root" + "@" + ip + " '" + command + "'")
+
+
+"""
+
+To get this program working you may have your servers able to be accessed via ssh-key. 
+Configure your ssh service accordinly. 
+
+
+"""
