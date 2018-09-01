4eaf3965e75266591805f9562a99ee6cefaf3aa2
Ricardo <barbosa.cisco@gmail.com>
2018-09-01 08:28:02 +0100

    added elif statement to lagartuz_input.py program

--- lagartuz/lagartuz_input.py
+++ lagartuz/lagartuz_input.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
-project = raw_input("What is your project? ")
+project = raw_input("What project are you working on? Lagartuz or Tartuz? ")
 
 #print project.title()
 
 if project == "Lagartuz":
 	print "Lagartuz Project is the best!"
+
+elif project == "Tartuz":
+	print "Tartuz is not so good as Lagartuz, ):"
 	
 else:
 
