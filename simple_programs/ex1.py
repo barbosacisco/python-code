510dd0f33d11445f014fa4c2ddcd16c71438e0ba
Ricardo Barbosa <Ricardo.Almeida_Barbosa@genesys.com>
2018-11-07 23:05:30 +0000

    re-organized folders and python programs

---
+++ simple_programs/ex1.py
@@ -0,0 +1,12 @@
+name = input("Hello, please type your name: ")
+
+if name != "":
+    age = int(input("Great, now please type your age: "))
+    diff = 100 - age
+    diff = str(diff)
+    message = "You will be 100 years old in " + diff + " years."
+    number = int(input("Please type another number: "))
+    print(message * number)
+else:
+    print("Sorry I need you to type your name in order to proceed. ")
+
---
+++ simple_programs/square.py
@@ -0,0 +1,5 @@
+my_numbers = [1,2,3,4,5,6,7,8,9,10]
+
+square = [num*num for num in my_numbers]
+
+print(square)
