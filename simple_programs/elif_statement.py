b7d7ac8ace19aa9b89d3f96f2c0bbe00b0130db0
Ricardo A. Barbosa <barbosa.cisco@gmail.com>
2018-11-07 22:54:05 +0000

    simple corrections performed

--- simple_programs/elif_statement.py
+++ simple_programs/elif_statement.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 
 """
+
 This is just a simple program to show the use of Flow Control ( Working with conditions, true or false ).
 Here we have two flow control statements, if , elif and else.
 
 "...if this is true do this, if not try this...else do this..." 
 
 """
-name = input("Type your name please: ") 
+name = input("Type your name please: ")
 
-if name == "John":                      
-	print ("Hello dear " + name + ", how have you been?")
+if name == "John":
+    print("Hello dear " + name + ", how have you been?")
 
 
 elif name == "Mary":
-	print ("Hum, " + name + " How have you been? I was expecting you!")
+    print("Hum, " + name + " How have you been? I was expecting you!")
 
 
-else:                                                      
-	print ("Oh I'm sorry " + name + " , I am expecting John and Mary.") 
+else:
+    print("Oh I'm sorry " + name + " , I am expecting John and Mary.")
 
 # Coding is awesome! (: 
 
-# Warning, make sure you execute this code with Python3, otherwise you will be presented with an error. 
+# Warning, make sure you execute this code with Python3, otherwise you will be presented with an error.
