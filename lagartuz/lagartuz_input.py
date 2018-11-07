50a14767fc2d5c065a2b562e3e100b7dffd2724e
Ricardo A. Barbosa <barbosa.cisco@gmail.com>
2018-11-07 23:43:04 +0000

    simple corrections

--- lagartuz/lagartuz_input.py
+++ lagartuz/lagartuz_input.py
@@ -1,16 +1,13 @@
-#!/usr/bin/python
 
-project = raw_input("What project are you working on? Lagartuz or Tartuz? ")
-
-#print project.title()
+project = input("What project are you working on? Lagartuz or Tartuz? ")
 
 if project == "Lagartuz":
-	print "Lagartuz Project is the best!"
+	print("Lagartuz Project is the best!")
 
 elif project == "Tartuz":
-	print "Tartuz is not so good as Lagartuz, ):"
+	print("Tartuz is not so good as Lagartuz")
 	
 else:
 
-	print "..."
+	print("...")
 
