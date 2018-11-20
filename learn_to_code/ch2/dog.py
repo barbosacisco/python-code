1eb070fdd641da714c2cb94bd3f6b72e6ffd2fa3
Ricardo <barbosa.cisco@gmail.com>
2018-11-20 22:29:03 +0000

    learn_to_code::book::oreilly::exercises

---
+++ learn_to_code/ch2/dog.py
@@ -0,0 +1,7 @@
+# This is the code from chapter 2 of Learn to Code from O'Reilly's book.
+
+dog_name = input("What is your dog's name? ")
+dog_age = input("What is your gog's age? ")
+human_age = int(dog_age) * 7
+print('Your dog', dog_name, 'is', human_age, 'years old in human years.')
+
