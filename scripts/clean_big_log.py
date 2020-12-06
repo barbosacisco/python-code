77fe83ee8c965bb8789c66eb236e035c08331525
Ricardo Barbosa <ricardo@flugel.it>
2020-12-06 17:59:05 +0000

    small fixes according to PEP8 standard

--- scripts/clean_big_log.py
+++ scripts/clean_big_log.py
@@ -3,7 +3,7 @@ import os
 file = "/your/log/file/path"
 statinfo = os.stat(file)
 
-if statinfo.st_size >= 10485760000:  #10GB file. 
+if statinfo.st_size >= 10485760000:  # 10GB file.
     os.system('echo "" >' + file)
-else :
-    print ("File still not large enough to be removed.")
+else:
+    print("File still not large enough to be removed.")
